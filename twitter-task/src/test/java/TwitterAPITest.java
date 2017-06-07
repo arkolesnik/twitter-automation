9249ce40fa0afed7af358265484aa314f419db1f
Olena Kolesnyk <a.r.kolesnik@gmail.com>
2017-06-08 00:58:45 +0300

    Refactored API tests

--- twitter-task/src/test/java/TwitterAPITest.java
+++ twitter-task/src/test/java/TwitterAPITest.java
@@ -1,24 +1,34 @@
 import api_helper.JSONResponse;
 import api_helper.Utils;
 import org.json.JSONObject;
-import org.junit.Assert;
-import org.junit.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class TwitterAPITest {
 
-    private static final String TEST_STATUS_CREATED_AT = "Tue Jun 06 16:25:54 +0000 2017";
-    private static final int TEST_STATUS_RETWEET_COUNT = 0;
-    private static final String TEST_STATUS_TEXT = "Test";
+    private static final String TEST_STATUS_CREATED_AT = "Wed Jun 07 16:28:17 +0000 2017";
+    private static final int TEST_STATUS_RETWEET_NUMBER = 0;
+    private static final String TEST_STATUS_TEXT = "MainTweet";
 
     private static final String UPDATE_STATUS_TEXT = "TestPostStatus";
 
     @Test
     public void testHomeTimeline() throws Exception {
         JSONObject statusTimeLine = Utils.getHomeTimeLine().getJSONArray().getJSONObject(0);
-
-        Assert.assertEquals(TEST_STATUS_CREATED_AT, statusTimeLine.getString("created_at"));
-        Assert.assertEquals(TEST_STATUS_RETWEET_COUNT, statusTimeLine.getInt("retweet_count"));
-        Assert.assertEquals(TEST_STATUS_TEXT, statusTimeLine.getString("text"));
+        StringBuilder error = new StringBuilder();
+        String actualDate = statusTimeLine.getString("created_at");
+        if (!TEST_STATUS_CREATED_AT.equalsIgnoreCase(actualDate)) {
+            error.append("Expected tweet text is " + TEST_STATUS_CREATED_AT + ", but actual is " + actualDate + "\n");
+        }
+        int actualRetweetNumber = statusTimeLine.getInt("retweet_count");
+        if (TEST_STATUS_RETWEET_NUMBER != actualRetweetNumber) {
+            error.append("Expected retweet number " + TEST_STATUS_RETWEET_NUMBER + ", but actual is " + actualRetweetNumber + "\n");
+        }
+        String actualTweetText = statusTimeLine.getString("text");
+        if (!TEST_STATUS_TEXT.equalsIgnoreCase(actualTweetText)) {
+            error.append("Expected tweet text is " + TEST_STATUS_TEXT + ", but actual is " + actualTweetText + "\n");
+        }
+        Assert.assertTrue(error.toString().isEmpty(), error.toString());
     }
 
     @Test
@@ -26,11 +36,9 @@ public class TwitterAPITest {
         JSONObject status = Utils.updateStatus(UPDATE_STATUS_TEXT).getJSONObject();
         long statusId = status.getLong("id");
         String statusText = status.getString("text");
-
-        try{
+        try {
             Assert.assertEquals(UPDATE_STATUS_TEXT, statusText);
-        }
-        finally {
+        } finally {
             Utils.destroyStatus(statusId);
         }
 
@@ -40,13 +48,10 @@ public class TwitterAPITest {
     public void testDoublePostStatus() throws Exception {
         JSONObject status = Utils.updateStatus(UPDATE_STATUS_TEXT).getJSONObject();
         long statusId = status.getLong("id");
-
         JSONResponse statusDouble = Utils.updateStatus(UPDATE_STATUS_TEXT);
-
-        try{
+        try {
             Assert.assertEquals(403, statusDouble.getStatusCode());
-        }
-        finally {
+        } finally {
             Utils.destroyStatus(statusId);
         }
     }
@@ -56,11 +61,8 @@ public class TwitterAPITest {
     public void testDestroyStatus() throws Exception {
         JSONObject status = Utils.updateStatus(UPDATE_STATUS_TEXT).getJSONObject();
         long statusId = status.getLong("id");
-
         Utils.destroyStatus(statusId);
-
-        JSONObject statusTimeLine = Utils.getHomeTimeLine().getJSONArray().getJSONObject(0); // loop
-
+        JSONObject statusTimeLine = Utils.getHomeTimeLine().getJSONArray().getJSONObject(0);
         Assert.assertNotEquals(statusId, statusTimeLine.getLong("id"));
     }
 }
